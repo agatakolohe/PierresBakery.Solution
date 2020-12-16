3c24b1afe9050391ba319bb57f62648bda1f968a
agatakolohe <agatakolohe@gmail.com>
2020-12-16 06:20:28 -0800

    purposely fail breadcost

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -16,6 +16,18 @@ namespace Bakery.Tests
         [TestMethod]
         public void BreadCost_GetBreadPrice_Int()
         {
+            int inputBreadQuantity = 1;
+            string inputBreadType = "rye";
+            int breadCostTest = 6;
+            Bread newBread = new Bread(inputBreadQuantity, inputBreadType);
+
+            int breadCost = newBread.BreadCost();
+
+            Assert.AreEqual(breadCostTest, breadCost);
+        }
+        [TestMethod]
+        public void BreadCost_GetBreadDealPrice_Int()
+        {
             int inputBreadQuantity = 3;
             string inputBreadType = "rye";
             int breadCostTest = 10;
